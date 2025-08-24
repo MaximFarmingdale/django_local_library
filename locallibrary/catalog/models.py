4f2cd76dd8b04826206809f7c6f217ea368e93ab
MaximFarmingdale <nikim@farmingdale.edu>
2025-08-24 16:22:39 -0400

    Added an Author Model

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -90,6 +90,21 @@ class BookInstance(models.Model):
         return f'{self.id} ({self.book.title})'
     class Meta:
         ordering = ['due_back']
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField(
+        'Died',
+        null=True,
+        blank=True,
+    )
+    def get_absolute_url(self):
+        return reverse("Author_detail", args=[str(self.id)])
+    def __str__(self):
+        return f'{self.first_name}, {self.last_name}'
+    class Meta:
+        ordering = ["last_name", "first_name"]
 
 
     
