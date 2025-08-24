8422fa68d6cc8e8cf4f5c396091bf56cd2fc562c
MaximFarmingdale <nikim@farmingdale.edu>
2025-08-24 16:21:22 -0400

    Added a Book Model

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -28,6 +28,35 @@ class Genre(models.Model):
             )
         ]
 
+class Book(models.Model): 
+
+    """Model for a book but not a specfic instance of said book"""
+    title = models.CharField( max_length=200, help_text="Enter the title of the book")
+    author = models.ForeignKey(
+        'Author', 
+        on_delete=models.RESTRICT, 
+        null=True,
+    )
+
+    summary = models.TextField(
+        max_length= 1000, 
+        help_text="Enter a brief description of the book",
+    )
+    isbn = models.CharField(
+        max_length=13,
+        unique=True,
+        help_text='Enter a 13 letter <a href = "https://www.isbn-international.org/content/what-isbn">ISBN</a>'
+    )
+    genre = models.ManyToManyField(
+        Genre, 
+        help_text="Enter a genre for this book",
+    )
+    def __str__(self):
+        return self.title
+    def get_absolute_url(self):
+        return reverse("book_detail", args=[str(self.id)])
+
+
 
     
 
