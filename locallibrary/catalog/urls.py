cc2c610ac24b3a358612f744d7bafd613333c1d4
MaximFarmingdale <nikim@farmingdale.edu>
2025-08-27 18:36:33 -0400

    added url pattern

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name= 'books'),
-    re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book_detail')
+    re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book_detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
 ]
