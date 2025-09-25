90fcc0b2abbe30f99e5b9a66f2a115014111bfb6
MaximFarmingdale <nikim@farmingdale.edu>
2025-09-25 16:43:22 -0400

    models.py: switched the order of last and first name in the string method of the author model

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -122,7 +122,7 @@ class Author(models.Model):
     def get_absolute_url(self):
         return reverse("author_detail", args=[str(self.id)])
     def __str__(self):
-        return f'{self.first_name}, {self.last_name}'
+        return f'{self.last_name}, {self.first_name}'
     class Meta:
         ordering = ["last_name", "first_name"]
 class Language(models.Model): 
